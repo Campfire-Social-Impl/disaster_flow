eeb86ba3093b3a48110f4bc040503f16e8894252
oyatomo <oyatomo.dev@gmail.com>
2024-10-11 11:18:17 +0900

    update: add first local db scheme

---
+++ lib/database.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+import 'package:sqlite3/sqlite3.dart';
+import 'package:sqlite3_flutter_libs/sqlite3_flutter_libs.dart';
+
+part 'database.g.dart';
+
+@DataClassName('flow')
+class FlowRaw extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  TextColumn get title => text()();
+  TextColumn get disaster => text()();
+}
+
+@DataClassName('action')
+class ActionRaw extends Table {
+  IntColumn get id => integer().autoIncrement()();
+  IntColumn get flowId => integer().references(FlowRaw, #id)();
+  TextColumn get title => text()();
+  TextColumn get action => text()();
+}
+
+@DriftDatabase(tables: [FlowRaw, ActionRaw])
+class LocalDatabase extends _$AppDatabase {
+  LocalDatabase() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 1;
+}
+
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFloder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFloder.path, 'db.sqlite'));
+    if (Platform.isAndroid) {
+      await applyWorkaroundToOpenSqlite3OnOldAndroidVersions();
+    }
+
+    final cachebase = (await getTemporaryDirectory()).path;
+    sqlite3.tempDirectory = cachebase;
+
+    return NativeDatabase.createInBackground(file);
+  });
+}

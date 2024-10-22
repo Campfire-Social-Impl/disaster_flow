ed6d00e42942e630af61c023e8e49456af69ebd8
oyatomo <82521012+Oya-Tomo@users.noreply.github.com>
2024-10-22 15:51:22 +0900

    update: removed bg color

--- lib/pages/flow_edit_page.dart
+++ lib/pages/flow_edit_page.dart
@@ -6,7 +6,56 @@ class FlowEditPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('防災フロー'),
+        centerTitle: true,
+        toolbarHeight: 80,
+        foregroundColor: Colors.black,
+        backgroundColor: const Color.fromARGB(255, 255, 220, 81),
+        shadowColor: Colors.black.withOpacity(0.2),
+        elevation: 20,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Card(
+              color: Colors.white,
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text("テーマ : 水害"),
+                        Text("アクション数 : 10 件"),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: 20,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Card(
+                    color: Colors.white,
+                    child: ListTile(
+                      title: Text('Item $index'),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }

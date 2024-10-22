c96bf74a99c627408c2c4586bb4b709b5a34c393
oyatomo <82521012+Oya-Tomo@users.noreply.github.com>
2024-10-22 19:26:02 +0900

    update: fix name of page

--- lib/pages/theme_choice_page.dart
+++ lib/pages/theme_choice_page.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class ThemeChoiceScreen extends HookConsumerWidget {
-  const ThemeChoiceScreen({super.key});
+class ThemeChoicePage extends HookConsumerWidget {
+  const ThemeChoicePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {

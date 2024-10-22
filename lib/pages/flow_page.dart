4a1107519fde48a23191a7a02177c27607a636e1
oyatomo <82521012+Oya-Tomo@users.noreply.github.com>
2024-10-22 13:50:57 +0900

    update: fixed args

--- lib/pages/flow_page.dart
+++ lib/pages/flow_page.dart
@@ -18,31 +18,67 @@ class FlowScreen extends StatelessWidget {
       body: ListView(
         padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 7),
         children: [
-          _buildCard(context, "assets/images/earthquake.png", "地震", () {}),
-          _buildCard(context, "assets/images/flood.png", "洪水", () {}),
+          _buildCard(
+            context,
+            path: "assets/images/earthquake.png",
+            title: "地震",
+            onTap: () {},
+          ),
+          _buildCard(
+            context,
+            path: "assets/images/flood.png",
+            title: "洪水",
+            onTap: () {},
+          ),
         ],
       ),
-      bottomNavigationBar: BottomAppBar(
-        color: const Color.fromARGB(255, 255, 255, 255),
-        height: 91,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            _buildBottomNavItem(Icons.newspaper, 'ニュース', () {}),
-            _buildBottomNavItem(Icons.home, 'ホーム', () {}),
-            _buildBottomNavItem(Icons.menu, 'その他', () {}),
-          ],
-        ),
+      bottomNavigationBar: BottomNavigationBar(
+        fixedColor: Colors.black,
+        unselectedItemColor: Colors.grey[700],
+        iconSize: 30,
+        backgroundColor: Colors.yellow[50],
+        items: const [
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.only(top: 8),
+              child: Icon(
+                Icons.newspaper,
+                color: Colors.black,
+              ),
+            ),
+            label: 'ニュース',
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.only(top: 8),
+              child: Icon(
+                Icons.home,
+                color: Colors.black,
+              ),
+            ),
+            label: 'ホーム',
+          ),
+          BottomNavigationBarItem(
+            icon: Padding(
+              padding: EdgeInsets.only(top: 8),
+              child: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+            ),
+            label: 'その他',
+          ),
+        ],
       ),
     );
   }
 
   Widget _buildCard(
-    BuildContext context,
-    String path,
-    String title,
+    BuildContext context, {
+    required String path,
+    required String title,
     void Function()? onTap,
-  ) {
+  }) {
     return Card(
       child: InkWell(
         onTap: onTap,
@@ -80,87 +116,6 @@ class FlowScreen extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-
-  // カードウィジェットを生成する関数
-  Widget _buildOptionCard(
-    BuildContext context, {
-    required String title,
-    required String imagePath,
-    required VoidCallback onTap,
-  }) {
-    return Container(
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.25), // 影の色
-            blurRadius: 10, // ぼかしの強さ
-            spreadRadius: 2, // 影の広がり
-            offset: Offset(0, 5), // 影の位置（X, Y方向）
-          ),
-        ],
-      ),
-      child: InkWell(
-        onTap: onTap,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(30), // 均等な角丸を適用
-          child: Stack(
-            alignment: Alignment.center,
-            children: [
-              Image.asset(
-                imagePath,
-                width: 400, // 幅を設定
-                height: 250, // 高さを設定
-                fit: BoxFit.cover, // 画像を均等に配置
-              ),
-              Align(
-                alignment: Alignment.center,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.black.withOpacity(0.5), // 背景色を黒に設定（透過あり）
-                    borderRadius: BorderRadius.circular(15), // 同じ角丸を適用
-                  ),
-                  width: double.infinity,
-                  height: 50,
-                  alignment: Alignment.center,
-                  child: Text(
-                    title,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-
-  // BottomNavigationBarのアイテムを生成する関数
-  Widget _buildBottomNavItem(IconData icon, String label, VoidCallback onTap) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        IconButton(
-          padding: EdgeInsets.zero,
-          iconSize: 30,
-          icon: Icon(icon, color: const Color.fromARGB(255, 0, 0, 0)),
-          onPressed: onTap,
-        ),
-        Text(
-          label,
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 13,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-      ],
     );
   }
 }

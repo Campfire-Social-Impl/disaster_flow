7191a95aa7657456e4b6050c007167a41e923e2f
oyatomo <82521012+Oya-Tomo@users.noreply.github.com>
2024-10-22 14:06:21 +0900

    update: removed unused bottom nav bar

--- lib/pages/flow_page.dart
+++ lib/pages/flow_page.dart
@@ -32,44 +32,6 @@ class FlowScreen extends StatelessWidget {
           ),
         ],
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        fixedColor: Colors.black,
-        unselectedItemColor: Colors.grey[700],
-        iconSize: 30,
-        backgroundColor: Colors.yellow[50],
-        items: const [
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.only(top: 8),
-              child: Icon(
-                Icons.newspaper,
-                color: Colors.black,
-              ),
-            ),
-            label: 'ニュース',
-          ),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.only(top: 8),
-              child: Icon(
-                Icons.home,
-                color: Colors.black,
-              ),
-            ),
-            label: 'ホーム',
-          ),
-          BottomNavigationBarItem(
-            icon: Padding(
-              padding: EdgeInsets.only(top: 8),
-              child: Icon(
-                Icons.menu,
-                color: Colors.black,
-              ),
-            ),
-            label: 'その他',
-          ),
-        ],
-      ),
     );
   }
 

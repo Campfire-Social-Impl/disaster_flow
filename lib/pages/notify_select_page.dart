31fa84f00f53dc4588b3bc78f170c5e5cb9e0168
oyatomo <82521012+Oya-Tomo@users.noreply.github.com>
2024-11-17 11:45:19 +0900

    update: add no data panel

--- lib/pages/notify_select_page.dart
+++ lib/pages/notify_select_page.dart
@@ -12,6 +12,7 @@ class NotifySelectPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final notifies = ref.watch(notifiesProvider).value;
     return Scaffold(
       appBar: AppBar(
         leading: IconButton(
@@ -20,40 +21,48 @@ class NotifySelectPage extends HookConsumerWidget {
           },
           icon: const Icon(Icons.arrow_back),
         ),
-        title: const Text('フローリスト'),
+        title: const Text('避難通知'),
         centerTitle: true,
         toolbarHeight: 80,
         foregroundColor: Colors.black,
         backgroundColor: const Color.fromARGB(255, 255, 220, 81),
         shadowColor: Colors.black.withOpacity(0.2),
       ),
-      body: ref.watch(notifiesProvider).value != null
-          ? ListView.builder(
-              itemCount: ref.watch(notifiesProvider).value!.length,
-              itemBuilder: (context, index) {
-                final notify = ref.read(notifiesProvider).value![index];
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 8.0, vertical: 2.0),
-                  child: Card(
-                    child: ListTile(
-                      leading: const Icon(Icons.directions_run),
-                      title: Text(notify.disaster),
-                      subtitle: Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Text(
-                          notify.description,
-                          style: TextStyle(
-                            color: Colors.brown[500],
+      body: notifies != null
+          ? (notifies.isNotEmpty
+              ? ListView.builder(
+                  itemCount: ref.watch(notifiesProvider).value!.length,
+                  itemBuilder: (context, index) {
+                    final notify = notifies[index];
+                    return Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8.0, vertical: 2.0),
+                      child: Card(
+                        child: ListTile(
+                          leading: const Icon(Icons.directions_run),
+                          title: Text(notify.disaster),
+                          subtitle: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              notify.description,
+                              style: TextStyle(
+                                color: Colors.brown[500],
+                              ),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                  ),
-                );
-              },
-            )
+                    );
+                  },
+                )
+              : noNotifyPanel(context))
           : loadingPanel(context),
+    );
+  }
+
+  Widget noNotifyPanel(BuildContext context) {
+    return const Center(
+      child: Text('避難通知がありません'),
     );
   }
 
